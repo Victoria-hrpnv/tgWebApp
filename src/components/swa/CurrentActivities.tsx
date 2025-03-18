220eadadb1c625856e30e5b384d1904d697d6410
vikah <vikahrapina00@mail.ru>
2025-03-18 13:17:28 +0300

    fix bug

--- src/components/swa/CurrentActivities.tsx
+++ src/components/swa/CurrentActivities.tsx
@@ -42,8 +42,6 @@ const CurrentActivities: FunctionComponent = () => {
     }
 
     return <div>
-        <div>{initDataRaw}
-        </div>
         <Divider
             style={{
                 border: 'none',
